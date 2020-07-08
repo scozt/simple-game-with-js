fc9292c3d5786a18b2adabc880cefd0426c65316
scozt <mr.oeztuerk@gmail.com>
2020-07-08 13:12:19 +0200

    add checkbox columns to each divs

--- javascript/main.js
+++ javascript/main.js
@@ -4,12 +4,20 @@ document.body.appendChild(div);
 
 const GAME = {
     ROWS: 10,
+    COLUMNS: 19,
 }
 
 for (let row = 0; row < GAME.ROWS; row++) {
     const div = document.createElement("div");
     div.className = "row";
     div.id = row;
+
+    for (let column = 0; column < GAME.COLUMNS; column++) {
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = column;
+        div.appendChild(checkbox);
+    }
 
     document.getElementById("container").appendChild(div);
 }
