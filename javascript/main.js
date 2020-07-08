749dec0f0ead815755c3d3ad80a59c7d0b50ffcf
scozt <mr.oeztuerk@gmail.com>
2020-07-08 13:09:16 +0200

    add row divs to the container div

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,15 @@
 const div = document.createElement("div");
 div.id = "container";
 document.body.appendChild(div);
+
+const GAME = {
+    ROWS: 10,
+}
+
+for (let row = 0; row < GAME.ROWS; row++) {
+    const div = document.createElement("div");
+    div.className = "row";
+    div.id = row;
+
+    document.getElementById("container").appendChild(div);
+}
